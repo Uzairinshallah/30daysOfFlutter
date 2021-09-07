320d440ddac82d6a5cd0b29937cc7e884949e04f
Uzair <uzairinshallah1@gmail.com>
2021-09-07 14:45:55 +0500

    theme extracted

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/pages/login_page.dart';
 import 'package:flutter_application_1/utils/routes.dart';
+import 'package:flutter_application_1/widgets/themes.dart';
 import 'pages/home_page.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -14,15 +15,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       //home: HomePage(),
       themeMode: ThemeMode.light,
-      theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
-        fontFamily: GoogleFonts.lato().fontFamily,
-        // primaryTextTheme: GoogleFonts.latoTextTheme(),
-      ),
+      theme: MyTheme.LightTheme(context), 
+      darkTheme: MyTheme.DarkTheme(context),
       debugShowCheckedModeBanner: false,
-      darkTheme: ThemeData(
-        brightness: Brightness.dark,
-      ),
       initialRoute: MyRoutes.homeRoute,
       routes: {
         "/": (context) => LoginPage(),
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,19 +3,19 @@ import 'package:flutter_application_1/widgets/drawer.dart';
 
 // ignore: must_be_immutable
 class HomePage extends StatelessWidget {
-  String name = "Uzair";
+  String name = 'Uzair';
   int days = 30;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Catalog App"),
+        title: Text('Catalog App'),
       ),
       body: Center(
         child: Container(
           child: Text(
-            "Welcome to the $days days of flutter by $name",
+            'Welcome to the $days days of flutter by $name',
           ),
         ),
       ),
